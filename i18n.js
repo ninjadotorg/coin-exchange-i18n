043f33db7274b6d7cd77622246ace7d044508028
0xmegalodon <dung.thanh@autonomous.nyc>
2018-11-28 15:35:56 +0700

    -: update code for new language

--- i18n.js
+++ i18n.js
@@ -1,7 +1,7 @@
 
 const id = require('./i18n/id.json');
 const ms = require('./i18n/ms.json');
-const zh_hk = require('./i18n/zh_hk.json');
+const zh = require('./i18n/zh.json');
 
 module.exports = {
   id, ms, zh_hk
