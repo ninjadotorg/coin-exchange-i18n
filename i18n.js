e71da83d2621a4b12f503bbfe8533d3c7c245bbc
Ken Trinh <39719217+0xbigfoot@users.noreply.github.com>
2019-01-16 15:03:25 +0700

    update vi language

--- i18n.js
+++ i18n.js
@@ -2,7 +2,8 @@
 const id = require('./i18n/id.json');
 const km = require('./i18n/km.json');
 const zh = require('./i18n/zh.json');
+const vi = require('./i18n/vi.json');
 
 module.exports = {
-  id, km, zh
+  id, km, zh, vi
 };
